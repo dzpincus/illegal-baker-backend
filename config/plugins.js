c4e4e9166becd32c5c202dd68e748be77445d1b9
Dylan Pincus <dpincus95@gmail.com>
2022-08-06 12:02:42 -0400

    Send emails on new orders

--- config/plugins.js
+++ config/plugins.js
@@ -12,5 +12,23 @@ module.exports = ({ env }) => ({
         delete: {},
       },
     },
+  },
+  email: {
+    config: {
+      provider: 'gmail-api',
+      providerOptions: {
+        auth: {
+          userId: env('EMAIL_OAUTH2_USERID'),
+          clientId: env('EMAIL_OAUTH2_CLIENTID'),
+          clientSecret: env('EMAIL_OAUTH2_CLIENTSECRET'),
+          refreshToken: env('EMAIL_OAUTH2_REFRESHTOKEN'),
+        },
+      },
+      settings: {
+        defaultFrom: env('EMAIL_FROM'),
+        defaultReplyTo: env('EMAIL_REPLYTO'),
+        testAddress: env('EMAIL_TEST_ADDRESS'),
+      },
+    },
   }
 });
--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -6,4 +6,44 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+  async create(ctx) {
+    const response = await super.create(ctx)
+    let order = JSON.parse(ctx.request.body.data);
+    console.log(order)
+    await strapi.plugin('email-designer')
+    .service('email')
+    .sendTemplatedEmail({
+      to: order.order_data.contactInfo.email,
+    },
+    {
+      templateReferenceId: 1,
+      subject: 'Illegal Baker - Order Confirmation'
+    }, 
+    {
+      order: order,
+      addressLabel: order.order_data.delivery.type === "pickup" ? "Pickup Address" : "Delivery Address",
+      address: order.order_data.delivery.type === "pickup" ? order.order_data.delivery.selectedPickupLocation : order.order_data.delivery.deliveryAddress,
+      payment: order.order_data.payment.type === "cash" ? "Cash on order pickup" : "Paid in full",
+      orderId: response.data.id
+    })
+
+    await strapi.plugin('email-designer')
+    .service('email')
+    .sendTemplatedEmail({
+      to: 'amy@illegalbaker.com',
+    },
+    {
+      templateReferenceId: 2,
+      subject: 'New Order!'
+    }, 
+    {
+      order: order,
+      addressLabel: order.order_data.delivery.type === "pickup" ? "Pickup Address" : "Delivery Address",
+      address: order.order_data.delivery.type === "pickup" ? order.order_data.delivery.selectedPickupLocation : order.order_data.delivery.deliveryAddress,
+      payment: order.order_data.payment.type === "cash" ? "Cash on order pickup" : "Paid in full",
+      orderId: response.data.id
+    })
+    return response;
+  }
+}));
